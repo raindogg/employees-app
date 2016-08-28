78d941fa4ed36fe9b876550dc7fbd8f69a6ef0e7
wabray17 <wabray17@gmail.com>
2016-08-28 16:40:42 -0500

    Bonus completed

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,4 +2,8 @@ class ContactsController < ApplicationController
   def first
     @contact = Contact.find(1)
   end
+
+  def all
+    @contacts = Contact.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/first' => 'contacts#first'
+  get '/all' => 'contacts#all'
 end
